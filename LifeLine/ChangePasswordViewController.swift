ae3b8f8ae0d209d57401d44dbfb4ddc8e77cba2e
Praveen Vandeyar <praveenvandeyar@gmail.com>
2020-04-15 13:35:29 -0700

    Fix user settings controllers

--- LifeLine/ChangePasswordViewController.swift
+++ LifeLine/ChangePasswordViewController.swift
@@ -10,18 +10,31 @@ import UIKit
 
 class ChangePasswordViewController: UIViewController {
 
-    @IBOutlet weak var oldPassword: UITextField!
-    @IBOutlet weak var newPassword: UITextField!
-    @IBOutlet weak var newPassword2: UITextField!
-    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var oldPasswordField: UITextField!
+    @IBOutlet weak var newPasswordField: UITextField!
+    @IBOutlet weak var newPassword2Field: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let dict = Archiver().getObject(fileName: "userinfo") as! NSDictionary
+        phoneLabel.text = (dict["phone"] as! String)
     }
     
     @IBAction func updatePassword(_ sender: Any) {
-        //LifeLineAPICaller().changePassword(phone: <#T##String#>, oldPassword: <#T##String#>, newPassword: <#T##String#>, resultLabel: <#T##UILabel#>)
+        let phone = phoneLabel.text!
+        let oldPassword = oldPasswordField.text!
+        let newPassword = newPasswordField.text!
+        let newPassword2 = newPassword2Field.text!
+        oldPasswordField.text = ""
+        newPasswordField.text = ""
+        newPassword2Field.text = ""
+        if (newPassword == newPassword2) {
+            LifeLineAPICaller().changePassword(phone: phone, oldPassword: oldPassword, newPassword: newPassword, resultLabel: resultLabel)
+        } else {
+            resultLabel.text = "Passwords do not match"
+        }
     }
     
 }
