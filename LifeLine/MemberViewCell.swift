f21f5b66f43e99a78c40fbaff3324568bd5a4df7
Praveen Vandeyar <praveenvandeyar@gmail.com>
2020-04-21 16:29:48 -0700

    Create group setting views

---
+++ LifeLine/MemberViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemberViewCell.swift
+//  LifeLine
+//
+//  Created by Praveen V on 4/21/20.
+//  Copyright © 2020 Praveen Vandeyar. All rights reserved.
+//
+
+import UIKit
+
+class MemberViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
