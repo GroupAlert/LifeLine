d969b5f40d02c1067d9a88e744f97b84c479ffbd
Stephen Baity <steve.baity@gmail.com>
2020-04-30 01:37:58 -0700

    Added functionality to: MembersView Controller; MemberViewCell by passing the groupID and getting the list of its members trough the API

--- LifeLine/MemberViewCell.swift
+++ LifeLine/MemberViewCell.swift
@@ -5,14 +5,28 @@
 //  Created by Praveen V on 4/21/20.
 //  Copyright © 2020 Praveen Vandeyar. All rights reserved.
 //
-
+import Alamofire
 import UIKit
 
 class MemberViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
+	@IBOutlet var picture: UIImageView!
+	@IBOutlet var nameLabel: UILabel!
+	@IBOutlet var bodyLabel: UILabel!
+	override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+		/* Get the picture 
+		let pictureUrl = URL(string: dict["picture"] as! String)!
+		Alamofire.request(pictureUrl).responseData { (response) in
+		if response.error == nil {
+			print(response.result)
+				if let data = response.data {
+					self.picture.image = UIImage(data: data)
+				}
+			}
+		}
+ */
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- LifeLine/MembersViewController.swift
+++ LifeLine/MembersViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class MembersViewController: UIViewController {
-
+class MembersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	
+	@IBOutlet var tableView: UITableView!
+	
+	var groupID = String()
+	var dict = [String:Any]()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		
+		tableView.dataSource = self
+		tableView.delegate = self
+		 
+		
+	let url = URL(string: LifeLineAPICaller().baseURL + "group/groupmembergetmembers.php")!
+			   var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+			   request.httpMethod = "POST"
+		let postString = "group_id=\(self.groupID)"
+			   request.httpBody = postString.data(using: String.Encoding.utf8)
+			   let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+			   let task = session.dataTask(with: request) { (data, response, error) in
+				  if let error = error {
+					 print(error.localizedDescription)
+				  } else if let data = data {
+				   let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+				   self.dict = dataDictionary
+				   self.tableView.reloadData()
+					}
+				  }
+			   task.resume()
         // Do any additional setup after loading the view.
+		
     }
+	
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		let size = dict["size"] as? Int ?? 0
+        return size
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "MemberViewCell") as! MemberViewCell
+		
+		let	groupMember = dict[String(indexPath.row)] as! [String:Any]
+		print(groupMember)
+		let name = groupMember["name"] as! String
+		let phone = groupMember["phone"] as! String
+		
+		cell.nameLabel.text = name
+		cell.bodyLabel.text = phone
+		
+		return cell
+	}
     
 
     /*
@@ -23,7 +69,7 @@ class MembersViewController: UIViewController {
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+	// Pass the selecte? object to the new view controller.
     }
     */
 
