429a22e8c2855ce409ace7a48f2ba053b79c924a
Praveen Vandeyar <praveenvandeyar@gmail.com>
2020-05-02 00:07:59 -0700

    fix

--- LifeLine/GroupSettingsViewController.swift
+++ LifeLine/GroupSettingsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 import AlamofireImage
 
 class GroupSettingsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
